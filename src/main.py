ca96396789fab1f0e5a9cb58e161ed8688182e60
agmopera <aixremin@gmail.com>
2026-08-02 03:14:35 +0300

    Render port fix

--- src/main.py
+++ src/main.py
@@ -954,8 +954,12 @@ def excel_rapor():
     )
 
 
-if __name__ == "__main__":
+import os
 
+if __name__ == "__main__":
     app.run(
-        debug=True
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
     )
+    
